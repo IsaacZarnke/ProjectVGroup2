8ef2b7dad48c45d93c91abfb36e3081adcb9d4f7
Eric Weng <Eweng3376@conestogac.on.ca>
2023-10-21 11:13:43 -0400

    Added Image tranformation code and Resources folder containing 1 product and 1 ad template

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+public class ImageToAdTransformer
+{
+	public Transformer()
+	{
+        Image imageBackground = Image.FromFile("ad template 1.png");
+        Image imageOverlay = Image.FromFile("spam.png");
+
+        Image img = new Bitmap(imageBackground.Width, imageBackground.Height);
+        using (Graphics gr = Graphics.FromImage(img))
+        {
+            gr.DrawImage(imageBackground, new Point(0, 0));
+            gr.DrawImage(imageOverlay, new Point(0, 0));
+        }
+        img.Save("output.png", ImageFormat.Png);
+    }
+}
